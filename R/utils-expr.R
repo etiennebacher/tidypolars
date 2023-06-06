40434629a29b4edcfa97f56cd2498e93f3223e48
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2023-06-06 19:13:07 +0200

    wip

--- R/utils-expr.R
+++ R/utils-expr.R
@@ -1,17 +1,27 @@
 #' Rearrange classic R expressions in Polars syntax
 
-rearrange_exprs <- function(data, expressions) {
-  lapply(seq_along(expressions), \(x) {
-    x_expr <- expressions[[x]]
-    var_name <- names(expressions)[x]
-    deparsed <- deparse(x_expr)
+rearrange_expr <- function(x) {
 
-    deparsed <- replace_vars_in_expr(data, x_expr, deparsed)
+  new_x <- x
 
-    # pl_funs <- regmatches(deparsed, gregexpr("pl\\_\\w+", deparsed))
+  # for each function that has an equivalent in polars, I should
+  # extract the inside of the function (which usually is the first arg) and put
+  # it in front.
 
-    deparsed
-  })
+  funs <- find_function_call_in_string(x)
+
+  for (f in funs$in_polars) {
+    full_f <- regmatches(
+      new_x,
+      gregexpr(paste0(f, "\\((?<text>[^,]*)\\)"), new_x, perl = TRUE)
+    )
+    full_f2 <- gsub(paste0("^", f, "\\("), "", full_f)
+    full_f2 <- gsub("\\)$", "", full_f2)
+    gsub(full_f, full_f2, new_x)
+  }
+
+
+  # Functions that don't have a polars equivalent should go in an apply() call
 }
 
 
@@ -28,3 +38,35 @@ replace_vars_in_expr <- function(data, expression, deparsed) {
   }
   deparsed
 }
+
+
+#' In a deparsed expression, find the function calls and show which has an
+#' equivalent in polars and which hasn't
+
+find_function_call_in_string <- function(x) {
+
+  p <- parse(
+    text = x,
+    keep.source = TRUE
+  )
+  p_d <- getParseData(p)
+
+  function_calls <- p_d[p_d$token %in% c("SYMBOL_FUNCTION_CALL", "'+'", "'-'"), "text"]
+
+  if ("col" %in% function_calls) {
+    char_before_call <- regmatches(p, gregexpr(".[col]", p))[[1]]
+    char_before_call <- char_before_call[char_before_call != ""]
+    if (all(substr(char_before_call, 1, 1) == "$")) {
+      function_calls <- function_calls[function_calls != "col"]
+    }
+  }
+
+  pl_exprs <- get_polars_expr()
+
+  list(
+    in_polars = function_calls[function_calls %in% pl_exprs$expr],
+    not_in_polars = function_calls[!function_calls %in% pl_exprs$expr]
+  )
+
+}
+
