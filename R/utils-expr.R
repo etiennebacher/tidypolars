bffdb2bc0ecd2d36b2feeef2fa1551479675a373
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2023-06-06 16:58:46 +0200

    some wip for summarize

--- R/utils-expr.R
+++ R/utils-expr.R
@@ -6,22 +6,25 @@ rearrange_exprs <- function(data, expressions) {
     var_name <- names(expressions)[x]
     deparsed <- deparse(x_expr)
 
-    vars_used <- unlist(lapply(x_expr, as.character))
-    vars_used <- unique(vars_used[which(vars_used %in% pl_colnames(data))])
-
-    browser()
-
-    for (i in vars_used) {
-      deparsed <- sub(i, "", deparsed, fixed = TRUE)
-      deparsed <- paste0("pl$col('", i, "')$", deparsed)
-      deparsed <- gsub("\\(\\s+,", "", deparsed)
-      deparsed <- gsub("\\s+,\\)", "", deparsed)
-    }
-
-    deparsed <- paste0(deparsed, "$alias('", var_name, "')")
+    deparsed <- replace_vars_in_expr(data, x_expr, deparsed)
 
     # pl_funs <- regmatches(deparsed, gregexpr("pl\\_\\w+", deparsed))
 
     deparsed
   })
+}
+
+
+
+#' In a deparsed expression, find the variable names and add pl$col() around
+#' them
+
+replace_vars_in_expr <- function(data, expression, deparsed) {
+  vars_used <- unlist(lapply(expression, as.character))
+  vars_used <- unique(vars_used[which(vars_used %in% pl_colnames(data))])
+
+  for (i in vars_used) {
+    deparsed <- gsub(i, paste0("pl$col('", i, "')"), deparsed)
+  }
+  deparsed
 }
