b6eb9c299a2383d9dd6113820c5b4444a4015738
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2023-06-06 09:56:33 +0200

    handle some special exprs in filter

--- R/filter.R
+++ R/filter.R
@@ -15,8 +15,21 @@ pl_filter <- function(data, ...) {
 
   # build the polars expression
   expr <- as.character(dots)
-  for (i in vars) {
-    expr <- gsub(i, paste0("pl\\$col('", i, "')"), expr)
+  for (i in seq_along(expr)) {
+    has_pl_special_filter <- grepl(
+      paste0(
+        "(", paste(pl_special_filter_expr(), collapse = "|"), ")\\("
+      ),
+      expr[i]
+    )
+
+    if (has_pl_special_filter) {
+      expr[i] <- reorder_filter_expr(expr[i])
+    }
+
+    for (v in vars) {
+      expr[i] <- gsub(v, paste0("pl\\$col('", v, "')"), expr[i])
+    }
   }
 
   expr <- paste(expr, collapse = " & ") |>
@@ -26,6 +39,36 @@ pl_filter <- function(data, ...) {
 }
 
 
+pl_special_filter_expr <- function() {
+  c(
+    "!is.nan",
+    "is.nan",
+    "!is.na",
+    "is.na",
+    "%in%"
+  )
+}
 
 
+reorder_filter_expr <- function(expr) {
+  is_negated <- startsWith(expr, "!")
+  if (is_negated) {
+    expr <- sub("^!", "", expr)
+  }
+  expr <- str2lang(expr)
 
+  var_name <- deparse(expr[[2]])
+  fn_name <- if (is_negated) {
+    paste0("!", deparse(expr[[1]]))
+  } else {
+    deparse(expr[[1]])
+  }
+  fn_name <- switch(
+    fn_name,
+    "!is.na" = "is_not_null",
+    "is.na" = "is_null",
+    "!is.nan" = "is_not_nan",
+    "is.nan" = "is_nan"
+  )
+  paste0(var_name, "$", fn_name, "()")
+}
--- inst/tinytest/test_filter.R
+++ inst/tinytest/test_filter.R
@@ -28,3 +28,38 @@ expect_dim(
   c(115, 5)
 )
 
+# Special filters
+
+iris2 <- iris
+iris2[c(3, 8, 58, 133), "Species"] <- NA
+pl_iris_2 <- pl$DataFrame(iris2)
+
+expect_dim(
+  pl_filter(pl_iris_2, is.na(Species)),
+  c(4, 5)
+)
+expect_dim(
+  pl_filter(pl_iris_2, !is.na(Species)),
+  c(146, 5)
+)
+expect_dim(
+  pl_filter(pl_iris_2, Species == "setosa", !is.na(Species)),
+  c(48, 5)
+)
+
+iris2 <- iris
+iris2[c(3, 8, 58, 133), "Sepal.Length"] <- NaN
+pl_iris_2 <- pl$DataFrame(iris2)
+
+expect_dim(
+  pl_filter(pl_iris_2, is.nan(Sepal.Length)),
+  c(4, 5)
+)
+expect_dim(
+  pl_filter(pl_iris_2, !is.nan(Sepal.Length)),
+  c(146, 5)
+)
+expect_dim(
+  pl_filter(pl_iris_2, Species == "setosa", !is.nan(Sepal.Length)),
+  c(48, 5)
+)
