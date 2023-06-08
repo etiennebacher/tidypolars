61bd0f422d804688d61b8f19f43abe47bedeffbf
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2023-06-08 16:43:38 +0200

    add separate()

---
+++ R/separate.R
@@ -0,0 +1,24 @@
+#' @export
+pl_separate <- function(data, col, into, sep = "[^[:alnum:]]+", remove = TRUE) {
+
+  check_polars_data(data)
+  col <- deparse(substitute(col))
+
+  into_len <- length(into) - 1
+  temp_id <- paste(sample(letters), collapse = "")
+
+  data <- data$
+    with_columns(
+      pl$col(col)$cast(pl$Utf8)$
+        str$split_exact(eval(sep), into_len)$
+        alias(eval(temp_id))$
+        struct$
+        rename_fields(eval(into))
+    )$unnest(temp_id)
+
+  if (isTRUE(remove)) {
+    data <- data$drop(col)
+  }
+
+  data
+}
---
+++ inst/tinytest/test_separate.R
@@ -0,0 +1,41 @@
+source("helpers.R")
+using("tidypolars")
+
+test <- pl$DataFrame(
+  x = c(NA, "x.y", "x.z", "y.z")
+)
+
+expect_equal(
+  pl_separate(test, x, into = c("foo", "foo2"), sep = ".") |>
+    pl_pull(foo) |>
+    to_r(),
+  c(NA, "x", "x", "y")
+)
+
+expect_equal(
+  pl_separate(test, x, into = c("foo", "foo2"), sep = ".") |>
+    pl_pull(foo2) |>
+    to_r(),
+  c(NA, "y", "z", "z")
+)
+
+
+test2 <- pl$DataFrame(
+  x = c(NA, "x y", "x z", "y z")
+)
+
+# TODO: test more extensively regex
+
+# expect_equal(
+#   pl_separate(test2, x, into = c("foo", "foo2")) |>
+#     pl_pull(foo) |>
+#     to_r(),
+#   c(NA, "x", "x", "y")
+# )
+#
+# expect_equal(
+#   pl_separate(test2, x, into = c("foo", "foo2")) |>
+#     pl_pull(foo2) |>
+#     to_r(),
+#   c(NA, "y", "z", "z")
+# )
