d7d528be5d3b58a93de8c335f01a29277f0108aa
etiennebacher <etienne.bacher@protonmail.com>
2023-06-22 14:40:03 +0200

    lazy: add tests for arrange()

--- R/polars.R
+++ R/polars.R
@@ -1,5 +1,5 @@
 #' @import polars
-#' @importFrom dplyr anti_join arrange bind_cols bind_rows collect distinct full_join group_by inner_join left_join mutate pull rename rename_with right_join select semi_join slice_tail slice_head summarize ungroup
+#' @importFrom dplyr anti_join arrange bind_cols bind_rows collect distinct full_join group_by inner_join left_join mutate pull rename rename_with right_join select semi_join slice_tail slice_head summarize summarise ungroup
 #' @importFrom tidyr drop_na fill pivot_longer pivot_wider replace_na separate unite
 #' @export
 NULL
