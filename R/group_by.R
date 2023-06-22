e604d103ea9918ad61ac86246209cd2c0bf8f3d0
etiennebacher <etienne.bacher@protonmail.com>
2023-06-22 19:31:43 +0200

    group_by: docs

--- R/group_by.R
+++ R/group_by.R
@@ -1,4 +1,29 @@
+#' Group by one or more variables
+#'
+#' Most data operations are done on groups defined by variables. `pl_group_by()`
+#' takes an existing Polars Data/LazyFrame and converts it into a grouped one
+#' where operations are performed "by group". `pl_ungroup()` removes grouping.
+#'
+#' @param data A Polars Data/LazyFrame
+#' @param ... Variables to group by (used in `pl_group_by()` only).
+#' @param maintain_order Maintain row order. This is the default but it can
+#' slow down the process with large datasets and it prevents the use of
+#' streaming.
+#'
 #' @export
+#' @examples
+#' by_cyl <- mtcars |>
+#'   as_polars() |>
+#'   pl_group_by(cyl)
+#'
+#' by_cyl
+#'
+#' by_cyl |> pl_summarise(
+#'   disp = mean(disp),
+#'   hp = mean(hp)
+#' )
+#' by_cyl |> pl_filter(disp == max(disp))
+#'
 
 pl_group_by <- function(data, ..., maintain_order = TRUE) {
   check_polars_data(data)
