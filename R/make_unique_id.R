619939d3f9d1f270660709113702e0600fe83e38
etiennebacher <etienne.bacher@protonmail.com>
2023-06-26 16:02:48 +0200

    add function `make_unique_id()`

---
+++ R/make_unique_id.R
@@ -0,0 +1,23 @@
+#' Create a column with unique id per row values
+#'
+#' @inheritParams pl_select
+#' @param new_col Name of the new column
+#'
+#' @export
+#' @examples
+#' mtcars |>
+#'   as_polars() |>
+#'   make_unique_id(am, gear)
+
+make_unique_id <- function(data, ..., new_col = "hash") {
+  check_polars_data(data)
+  if ("hash" %in% pl_colnames(data)) {
+    stop("Column `hash` already exists. Please use a new name for the argument `new_col`.")
+  }
+  vars <- .select_nse_from_dots(data, ...)
+  if (length(vars) == 0) vars <- pl_colnames(data)
+  data$with_columns(
+    pl$struct(vars)$hash()$alias(new_col)
+  )
+}
+
