9992d9553f81bbb2465a6a3bea643f6082bcd654
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2023-06-29 09:52:51 +0200

    tinytest: docs

--- R/tinytest.R
+++ R/tinytest.R
@@ -1,18 +1,22 @@
+#' `tinytest` helper
+#'
 #' @export
 #' @keywords internal
 expect_colnames <- function(x, y) {
   res <- identical(x$columns, y)
-  tinytest(
+  tinytest::tinytest(
     result = res,
     call = sys.call(sys.parent(1))
   )
 }
 
+#' `tinytest` helper
+#'
 #' @export
 #' @keywords internal
 expect_dim <- function(x, y) {
   res <- identical(dim(x), y)
-  tinytest(
+  tinytest::tinytest(
     result = res,
     call = sys.call(sys.parent(1))
   )
