4db8e85472504d83717629e86bffa5f9b1a11f3a
etiennebacher <etienne.bacher@protonmail.com>
2023-07-24 14:59:37 +0200

    tests: add some benchmark, close #9

--- tests/tinytest/helpers.R
+++ tests/tinytest/helpers.R
@@ -7,4 +7,23 @@ register_tinytest_extension(
     "expect_error_lazy")
 )
 
+exit_if <- function(...) {
+  L <- as.list(substitute(list(...))[-1])
+  msg <- NULL
+  for (e in L) {
+    if (isTRUE(eval(e))) {
+      str <- paste0(deparse(e), collapse = " ")
+      msg <- sprintf("'%s' is TRUE", str)
+      break
+    }
+  }
+  msg
+}
 
+is_ci <- function() {
+  isTRUE(as.logical(Sys.getenv("CI", "false")))
+}
+
+is_cran <- function() {
+  isFALSE(as.logical(Sys.getenv("NOT_CRAN", "false")))
+}
---
+++ tests/tinytest/test_benchmark.R
@@ -0,0 +1,74 @@
+source("helpers.R")
+using("tidypolars")
+
+exit_if(is_ci(), is_cran())
+exit_file("Only for releases")
+
+tmp <- rep(list(mtcars), 2*1e5)
+
+# EAGER
+
+dat <- data.table::rbindlist(tmp) |>
+  as_polars()
+
+x <- bench::mark(
+  polars = dat$
+    select(c("mpg", "cyl", "disp", "vs", "am", "gear"))$
+    with_columns(
+      pl$when(
+        (pl$col("cyl") >= 6)
+      )$then("large")$
+        otherwise("small")$
+        alias("cyl_type")
+    )$
+    filter(pl$col("disp") / pl$col("gear") > 40),
+  tidypolars = dat |>
+    pl_select(c("mpg", "cyl", "disp", "vs", "am", "gear")) |>
+    pl_mutate(
+      cyl_type = ifelse(cyl >= 6, "large", "small")
+    ) |>
+    pl_filter(disp / gear > 40),
+  iterations = 5
+)
+
+times <- as.numeric(x$median)
+time_ratio <- times[2] / times[1]
+
+expect_true(
+  time_ratio < 1.05
+)
+
+
+# LAZY
+
+dat <- data.table::rbindlist(tmp) |>
+  as_polars(lazy = TRUE)
+
+x <- bench::mark(
+  polars = dat$
+    select(c("mpg", "cyl", "disp", "vs", "am", "gear"))$
+    with_columns(
+      pl$when(
+        (pl$col("cyl") >= 6)
+      )$then("large")$
+        otherwise("small")$
+        alias("cyl_type")
+    )$
+    filter(pl$col("disp") / pl$col("gear") > 40)$
+    collect(),
+  tidypolars = dat |>
+    pl_select(c("mpg", "cyl", "disp", "vs", "am", "gear")) |>
+    pl_mutate(
+      cyl_type = ifelse(cyl >= 6, "large", "small")
+    ) |>
+    pl_filter(disp / gear > 40) |>
+    pl_collect(),
+  iterations = 5
+)
+
+times <- as.numeric(x$median)
+time_ratio <- times[2] / times[1]
+
+expect_true(
+  time_ratio < 1.05
+)
