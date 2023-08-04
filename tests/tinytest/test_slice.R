c496130672832ff9a383758a5b28e1dc83238ceb
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2023-08-04 15:40:06 +0200

    fix slice_* per group

--- tests/tinytest/test_slice.R
+++ tests/tinytest/test_slice.R
@@ -14,3 +14,35 @@ expect_equal(
   tail(iris, n = 5),
   check.attributes = FALSE
 )
+
+pl_iris_g <- pl_iris |>
+  pl_group_by(Species)
+
+hd <- pl_slice_head(pl_iris_g, n = 2)
+
+expect_equal(
+  pl_pull(hd, Species),
+  factor(rep(c("setosa", "versicolor", "virginica"), each = 2))
+)
+
+expect_dim(hd, c(6, 5))
+
+expect_equal(
+  pl_pull(hd, Sepal.Length)[3:4],
+  c(7, 6.4)
+)
+
+
+tl <- pl_slice_tail(pl_iris_g, n = 2)
+
+expect_equal(
+  pl_pull(tl, Species),
+  factor(rep(c("setosa", "versicolor", "virginica"), each = 2))
+)
+
+expect_dim(tl, c(6, 5))
+
+expect_equal(
+  pl_pull(tl, Sepal.Length)[3:4],
+  c(5.1, 5.7)
+)
--- tests/tinytest/test_slice_lazy.R
+++ tests/tinytest/test_slice_lazy.R
@@ -19,4 +19,36 @@ expect_equal_lazy(
   check.attributes = FALSE
 )
 
+pl_iris_g <- pl_iris |>
+  pl_group_by(Species)
+
+hd <- pl_slice_head(pl_iris_g, n = 2)
+
+expect_equal_lazy(
+  pl_pull(hd, Species),
+  factor(rep(c("setosa", "versicolor", "virginica"), each = 2))
+)
+
+expect_dim(hd, c(6, 5))
+
+expect_equal_lazy(
+  pl_pull(hd, Sepal.Length)[3:4],
+  c(7, 6.4)
+)
+
+
+tl <- pl_slice_tail(pl_iris_g, n = 2)
+
+expect_equal_lazy(
+  pl_pull(tl, Species),
+  factor(rep(c("setosa", "versicolor", "virginica"), each = 2))
+)
+
+expect_dim(tl, c(6, 5))
+
+expect_equal_lazy(
+  pl_pull(tl, Sepal.Length)[3:4],
+  c(5.1, 5.7)
+)
+
 Sys.setenv('TIDYPOLARS_TEST' = FALSE)
