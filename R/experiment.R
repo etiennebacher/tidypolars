3b5016d0491ffdfc8532afa16d5974c3e2c36b30
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2023-08-07 17:49:01 +0200

    add explanations [skip ci]

--- R/experiment.R
+++ R/experiment.R
@@ -1,6 +1,32 @@
+# This file implements a DSL to translate each expression passed in mutate (or
+# summarize) to a Polars expression.
+
+# The function `polars_env()` is where we dispatch the rewriting process
+# depending on the type of code that is passed. It relies on nested environments,
+# the most nested one being the preferred one. There are 3 environments: symbols,
+# known functions, and unknown functions.
+
+# `call_env`:
+#     - all functions that are unknown
+#     - `f_env`:
+#           - functions that are in a "whitelist" defined by me. If the called
+#             function belongs to `f_env`, it is automatically replaced.
+#           - `symbol_env`:
+#                 - all symbols that are in a "whitelist". Same as `f_env` but
+#                   for symbols.
+#
+# When we pass the expression, each component passes through `polars_env`.
+# First, we check whether it is a symbol e.g Petal.Length (since `symbol_env`
+# is the most nested environment, we start by it). If it is one, we check if it's
+# a column name.
+# If it's not a symbol, then we go to the environment above (`f_env`) where we
+# check whether it's a whitelisted function. If so, we prefix it with `pl_`,
+# e.g "mean" -> "pl_mean".
+# Finally, if it's not a symbol or a known function, it means that it is an
+# unknown function, so we pass it through apply() with a warning about efficiency.
+
 
 polars_env <- function(expr, .data) {
-
   # Unknown functions
   calls <- all_calls(expr)
   call_list <- map(set_names(calls), unknown_op)
@@ -14,18 +40,20 @@ polars_env <- function(expr, .data) {
   if (length(names) == 1 && names %in% pl_colnames(.data)) {
     names <- set_names(paste0("pl$col('", names, "')"), names)
   } else {
-    names <- set_names(names)
+    names <- NULL
   }
   symbol_env <- as_environment(names, parent = f_env)
 
   symbol_env
 }
 
+# "rec" for "recursive"
 all_names_rec <- function(x) {
-  switch_expr(x,
-              constant = character(),
-              symbol =   as.character(x),
-              call =     flat_map_chr(as.list(x[-1]), all_names)
+  switch_expr(
+    x,
+    constant = character(),
+    symbol =   as.character(x),
+    call =     flat_map_chr(as.list(x[-1]), all_names)
   )
 }
 
@@ -53,7 +81,6 @@ binary_op <- function(sep) {
   )
 }
 
-# Binary operators
 f_env <- child_env(
   .parent = empty_env(),
   `+` = binary_op(" + "),
@@ -76,14 +103,15 @@ unknown_op <- function(op) {
 }
 
 all_calls_rec <- function(x) {
-  switch_expr(x,
-              constant = ,
-              symbol =   character(),
-              call = {
-                fname <- as.character(x[[1]])
-                children <- flat_map_chr(as.list(x[-1]), all_calls)
-                c(fname, children)
-              }
+  switch_expr(
+    x,
+    constant = ,
+    symbol =   character(),
+    call = {
+      fname <- as.character(x[[1]])
+      children <- flat_map_chr(as.list(x[-1]), all_calls)
+      c(fname, children)
+    }
   )
 }
 all_calls <- function(x) {
