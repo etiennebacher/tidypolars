5768b05b54e5f2c328395179bc94a76fc0930b00
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2023-08-09 14:09:25 +0200

    split funs in several files

---
+++ R/funs-date.R
@@ -0,0 +1,197 @@
+pl_default_round <- function(x, ...) {
+  check_empty_dots(...)
+  x$default$round()
+}
+
+pl_round <- function(x, ...) {
+  check_empty_dots(...)
+  x$dt$round()
+}
+
+pl_cast_time_unit <- function(x, ...) {
+  check_empty_dots(...)
+  x$dt$cast_time_unit()
+}
+
+pl_combine <- function(x, ...) {
+  check_empty_dots(...)
+  x$dt$combine()
+}
+
+pl_convert_time_zone <- function(x, ...) {
+  check_empty_dots(...)
+  x$dt$convert_time_zone()
+}
+
+pl_day <- function(x, ...) {
+  check_empty_dots(...)
+  x$dt$day()
+}
+
+pl_mday <- pl_day
+
+pl_yday <- function(x, ...) {
+  x$dt$ordinal_day()
+}
+
+pl_days <- function(x, ...) {
+  check_empty_dots(...)
+  x$dt$days()
+}
+
+pl_epoch <- function(x, ...) {
+  check_empty_dots(...)
+  x$dt$epoch()
+}
+
+pl_hour <- function(x, ...) {
+  check_empty_dots(...)
+  x$dt$hour()
+}
+
+pl_hours <- function(x, ...) {
+  check_empty_dots(...)
+  x$dt$hours()
+}
+
+pl_iso_year <- function(x, ...) {
+  check_empty_dots(...)
+  x$dt$iso_year()
+}
+
+pl_microsecond <- function(x, ...) {
+  check_empty_dots(...)
+  x$dt$microsecond()
+}
+
+pl_microseconds <- function(x, ...) {
+  check_empty_dots(...)
+  x$dt$microseconds()
+}
+
+pl_millisecond <- function(x, ...) {
+  check_empty_dots(...)
+  x$dt$millisecond()
+}
+
+pl_milliseconds <- function(x, ...) {
+  check_empty_dots(...)
+  x$dt$milliseconds()
+}
+
+pl_minute <- function(x, ...) {
+  check_empty_dots(...)
+  x$dt$minute()
+}
+
+pl_minutes <- function(x, ...) {
+  check_empty_dots(...)
+  x$dt$minutes()
+}
+
+pl_month <- function(x, ...) {
+  check_empty_dots(...)
+  x$dt$month()
+}
+
+pl_nanosecond <- function(x, ...) {
+  check_empty_dots(...)
+  x$dt$nanosecond()
+}
+
+pl_nanoseconds <- function(x, ...) {
+  check_empty_dots(...)
+  x$dt$nanoseconds()
+}
+
+pl_offset_by <- function(x, ...) {
+  check_empty_dots(...)
+  x$dt$offset_by()
+}
+
+pl_ordinal_day <- function(x, ...) {
+  check_empty_dots(...)
+  x$dt$ordinal_day()
+}
+
+pl_quarter <- function(x, ...) {
+  check_empty_dots(...)
+  x$dt$quarter()
+}
+
+pl_replace_time_zone <- function(x, ...) {
+  check_empty_dots(...)
+  x$dt$replace_time_zone()
+}
+
+pl_default_round <- function(x, ...) {
+  check_empty_dots(...)
+  x$default$round()
+}
+
+pl_round <- function(x, ...) {
+  check_empty_dots(...)
+  x$dt$round()
+}
+
+pl_second <- function(x, ...) {
+  check_empty_dots(...)
+  x$dt$second()
+}
+
+pl_seconds <- function(x, ...) {
+  check_empty_dots(...)
+  x$dt$seconds()
+}
+
+pl_strftime <- function(x, ...) {
+  check_empty_dots(...)
+  x$dt$strftime()
+}
+
+pl_timestamp <- function(x, ...) {
+  check_empty_dots(...)
+  x$dt$timestamp()
+}
+
+pl_truncate <- function(x, ...) {
+  check_empty_dots(...)
+  x$dt$truncate()
+}
+
+pl_tz_localize <- function(x, ...) {
+  check_empty_dots(...)
+  x$dt$tz_localize()
+}
+
+pl_week <- function(x, ...) {
+  check_empty_dots(...)
+  x$dt$week()
+}
+
+# TODO: check the day of weekstart (lubridate starts the
+# week on sunday, polars on monday)
+pl_weekday <- function(x, ...) {
+  check_empty_dots(...)
+  x$dt$weekday()
+}
+
+pl_with_time_unit <- function(x, ...) {
+  check_empty_dots(...)
+  x$dt$with_time_unit()
+}
+
+pl_year <- function(x, ...) {
+  check_empty_dots(...)
+  x$dt$year()
+}
+
+pl_ymd_hms <- function(x, ...) {
+  check_empty_dots(...)
+  x$str$strptime(pl$Datetime("ms"), "%Y-%m-%d %H:%M:%S", strict = FALSE)
+}
+
+pl_ymd_hm <- function(x, ...) {
+  check_empty_dots(...)
+  x$str$strptime(pl$Datetime("ms"), "%Y-%m-%d %H:%M", strict = FALSE)
+}
---
+++ R/funs-string.R
@@ -0,0 +1,175 @@
+
+pl_str_concat <- function(x, ...) {
+  check_empty_dots(...)
+  x$str$concat()
+}
+
+pl_str_contains <- function(x, ...) {
+  check_empty_dots(...)
+  x$str$contains()
+}
+
+pl_str_count_match <- function(x, pattern = "", ...) {
+  check_empty_dots(...)
+  x$str$count_match(pattern)
+}
+
+pl_str_decode <- function(x, ...) {
+  check_empty_dots(...)
+  x$str$decode()
+}
+
+pl_str_encode <- function(x, ...) {
+  check_empty_dots(...)
+  x$str$encode()
+}
+
+pl_str_ends <- function(x, pattern, negate = FALSE) {
+  if (isTRUE(negate)) {
+    x$str$ends_with(pl$lit(pattern))$is_not()
+  } else {
+    x$str$ends_with(pl$lit(pattern))
+  }
+}
+
+pl_str_extract <- function(x, pattern, group = 0) {
+  x$str$extract(pattern, group)
+}
+
+pl_str_extract_all <- function(x, ...) {
+  check_empty_dots(...)
+  x$str$extract_all()
+}
+
+pl_str_json_extract <- function(x, ...) {
+  check_empty_dots(...)
+  x$str$json_extract()
+}
+
+pl_str_json_path_match <- function(x, ...) {
+  check_empty_dots(...)
+  x$str$json_path_match()
+}
+
+pl_str_lengths <- function(x, ...) {
+  check_empty_dots(...)
+  x$str$lengths()
+}
+
+pl_str_ljust <- function(x, ...) {
+  check_empty_dots(...)
+  x$str$ljust()
+}
+
+pl_str_lstrip <- function(x, ...) {
+  check_empty_dots(...)
+  x$str$lstrip()
+}
+
+pl_str_length <- function(x, ...) {
+  check_empty_dots(...)
+  x$str$n_chars()
+}
+
+pl_str_n_chars <- pl_str_length
+
+pl_str_parse_int <- function(x, ...) {
+  check_empty_dots(...)
+  x$str$parse_int()
+}
+
+pl_str_replace <- function(x, pattern, replacement, ...) {
+  check_empty_dots(...)
+  x$str$replace(pattern, replacement)
+}
+
+pl_str_replace_all <- function(x, pattern, replacement, ...) {
+  check_empty_dots(...)
+  x$str$replace_all(pattern, replacement)
+}
+
+pl_str_rjust <- function(x, ...) {
+  check_empty_dots(...)
+  x$str$rjust()
+}
+
+pl_str_rstrip <- function(x, ...) {
+  check_empty_dots(...)
+  x$str$rstrip()
+}
+
+pl_str_slice <- function(x, start, end = NULL, ...) {
+  check_empty_dots(...)
+  # polars is 0-indexed
+  if (start > 0) start <- start - 1
+  x$str$slice(start, end)
+}
+
+pl_str_split <- function(x, ...) {
+  check_empty_dots(...)
+  x$str$split()
+}
+
+pl_str_split_exact <- function(x, ...) {
+  check_empty_dots(...)
+  x$str$split_exact()
+}
+
+pl_str_splitn <- function(x, ...) {
+  check_empty_dots(...)
+  x$str$splitn()
+}
+
+pl_str_starts <- function(x, pattern, negate = FALSE) {
+  if (isTRUE(negate)) {
+    x$str$starts_with(pl$lit(pattern))$is_not()
+  } else {
+    x$str$starts_with(pl$lit(pattern))
+  }
+}
+
+pl_str_str_explode <- function(x, ...) {
+  check_empty_dots(...)
+  x$str$str_explode()
+}
+
+pl_str_strip <- function(x, ...) {
+  check_empty_dots(...)
+  x$str$strip()
+}
+
+pl_str_strptime <- function(x, ...) {
+  check_empty_dots(...)
+  x$str$strptime()
+}
+
+pl_str_to_lower <- function(x, ...) {
+  check_empty_dots(...)
+  x$str$to_lowercase()
+}
+
+pl_tolower <- pl_str_to_lower
+
+pl_str_to_upper <- function(x, ...) {
+  check_empty_dots(...)
+  x$str$to_uppercase()
+}
+
+pl_toupper <- pl_str_to_upper
+
+pl_str_zfill <- function(x, ...) {
+  check_empty_dots(...)
+  x$str$zfill()
+}
+
+# not in polars
+pl_str_remove <- function(x, pattern, ...) {
+  check_empty_dots(...)
+  x$str$replace(pattern, "")
+}
+
+# not in polars
+pl_str_remove_all <- function(x, pattern, ...) {
+  check_empty_dots(...)
+  x$str$replace_all(pattern, "")
+}
