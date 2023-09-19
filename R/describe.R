bb8bb7dbdb3d2d5e4bd6bfd757a703afb0477389
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2023-09-19 13:48:24 +0200

    funs: support describe()

---
+++ R/describe.R
@@ -0,0 +1,23 @@
+#' Summary statistics for a Polars DataFrame
+#'
+#' @inheritParams pl_select
+#' @param percentiles One or more percentiles to include in the summary
+#' statistics. All values must be between 0 and 1 (`NULL` are ignored)⁠.
+#'
+#' @export
+#' @examples
+#' mtcars |>
+#'   as_polars() |>
+#'   describe(percentiles = c(0.2, 0.4, 0.6, 0.8))
+
+describe <- function(.data, percentiles = c(0.25, 0.75)) {
+  check_polars_data(.data)
+  between_zero_one <- percentiles >= 0 & percentiles <= 1
+  if (anyNA(between_zero_one) || any(!between_zero_one)) {
+    abort(
+      "All values of `percentiles` must be between 0 and 1."
+    )
+  }
+  .data$describe(percentiles)
+}
+
