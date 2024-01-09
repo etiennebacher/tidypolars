704b03ec5c4fdbc2421cf63291560012cb48fe54
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2024-01-09 07:18:20 +0100

    Add argument `.keep` in `mutate()` (#80)
    * init
    
    * finish tests, document
    
    * bump news

--- R/mutate.R
+++ R/mutate.R
@@ -14,19 +14,31 @@
 #' @param .by Optionally, a selection of columns to group by for just this
 #'   operation, functioning as an alternative to `group_by()`. The group order
 #'   is not maintained, use `group_by()` if you want more control over it.
+#' @param .keep Control which columns from `.data` are retained in the output.
+#' Grouping columns and columns created by `...` are always kept.
+#' * `"all"` retains all columns from .data. This is the default.
+#' * `"used"` retains only the columns used in ... to create new columns. This is
+#'   useful for checking your work, as it displays inputs and outputs side-by-
+#'   side.
+#' * `"unused"` retains only the columns not used in `...` to create new columns.
+#'   This is useful if you generate new columns, but no longer need the columns
+#'   used to generate them.
+#' * `"none"` doesn't retain any extra columns from `.data`. Only the grouping
+#'   variables and columns created by `...` are kept.
 #'
 #' @details
 #'
-#' A lot of functions available in base R (cos, sin, multiplying, etc.) or
+#' A lot of functions available in base R (cos, mean, multiplying, etc.) or
 #' in other packages (dplyr::lag(), etc.) are implemented in an efficient
-#' way in Polars. These functions will be automatically translated to Polars
+#' way in Polars. These functions are automatically translated to Polars
 #' syntax under the hood so that you can continue using the classic R syntax and
 #' functions.
 #'
-#' If a Polars built-in replacement doesn't exist (for example for custom
-#' functions), the R function will be passed to `map()` in the Polars workflow.
-#' Note that this is slower than using functions that can be translated to
-#' Polars syntax.
+# TODO: not true for now
+# If a Polars built-in replacement doesn't exist (for example for custom
+# functions), the R function will be passed to `map()` in the Polars workflow.
+# Note that this is slower than using functions that can be translated to
+# Polars syntax.
 #'
 #' @export
 #' @examplesIf require("dplyr", quietly = TRUE) && require("tidyr", quietly = TRUE)
@@ -70,9 +82,15 @@
 #' some_value <- 1
 #' mutate(pl_iris, x = {{ some_value }})
 
-mutate.RPolarsDataFrame <- function(.data, ..., .by = NULL) {
+mutate.RPolarsDataFrame <- function(
+    .data,
+    ...,
+    .by = NULL,
+    .keep = c("all", "used", "unused", "none")
+  ) {
 
   check_polars_data(.data)
+  .keep <- rlang::arg_match0(.keep, values = c("all", "used", "unused", "none"))
 
   grps <- get_grps(.data, rlang::enquo(.by), env = rlang::current_env())
   mo <- attributes(.data)$maintain_grp_order
@@ -81,10 +99,20 @@ mutate.RPolarsDataFrame <- function(.data, ..., .by = NULL) {
 
   polars_exprs <- translate_dots(.data = .data, ..., env = rlang::current_env())
 
+  used <- c()
+  orig_names <- names(.data)
+
   for (i in seq_along(polars_exprs)) {
     sub <- polars_exprs[[i]]
     to_drop <- names(empty_elems(sub))
     sub <- compact(sub)
+
+    used <- c(
+      used,
+      lapply(sub, \(x) x$meta$root_names()) |>
+        unlist() |>
+        unique()
+    )
 
     if (length(sub) > 0) {
       if (is_grouped) {
@@ -95,6 +123,19 @@ mutate.RPolarsDataFrame <- function(.data, ..., .by = NULL) {
 
     if (length(to_drop) > 0) {
       .data <- .data$drop(to_drop)
+    }
+  }
+
+  if (.keep != "all") {
+    new_vars <- setdiff(names(.data), orig_names)
+    not_used <- setdiff(orig_names, used)
+    not_used <- setdiff(not_used, grps)
+    if (.keep == "used") {
+      .data <- .data$drop(not_used)
+    } else if (.keep == "unused") {
+      .data <- .data$drop(used)
+    } else if (.keep == "none") {
+      .data <- .data$drop(c(not_used, used))
     }
   }
 
