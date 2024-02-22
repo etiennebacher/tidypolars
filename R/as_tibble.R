dc7c8885998a48a5bb15086d3acb76237c5b805d
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2024-02-22 16:20:33 +0100

    remove as.data.frame custom method

---
+++ R/as_tibble.R
@@ -0,0 +1,35 @@
+#' Convert a Polars DataFrame to an R data.frame or to a tibble
+#'
+#' This makes it easier to convert a polars [DataFrame][DataFrame_class] or
+#' [LazyFrame][LazyFrame_class] to a [`tibble`][tibble::tibble] in a pipe
+#' workflow.
+#'
+#' @inheritParams count.RPolarsDataFrame
+#' @param int64_conversion How should Int64 values be handled when converting a
+#'   polars object to R? See the documentation in
+#'   [`polars::as.data.frame.RPolarsDataFrame`].
+#' @param ... Not used.
+#'
+#' @section About int64: Int64 is a format accepted in Polars but not natively
+#'   in R (the package `bit64` helps with that).
+#'
+#'   Since `tidypolars` is simply a wrapper around `polars`, the behavior of
+#'   `int64` values will depend on the options set in `polars`. Use
+#'   `options(polars.int64_conversion =)` to specify how int64 variables should
+#'   be handled. See the [documentation in
+#'   `polars`](https://rpolars.github.io/man/polars_options.html#details) for
+#'   the possible options.
+#'
+#' @export
+#' @examplesIf require("dplyr", quietly = TRUE) && require("tidyr", quietly = TRUE)
+#' iris |>
+#'   as_polars_df() |>
+#'   filter(Sepal.Length > 6) |>
+#'   as_tibble()
+as_tibble.tidypolars <- function(
+    x,
+    int64_conversion = polars::polars_options()$int64_conversion,
+    ...
+  ) {
+  dplyr::as_tibble(as.data.frame(x, ...))
+}
