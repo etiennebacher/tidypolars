9b052a0ece3eb8d838550d210e809db4f33b1f21
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2024-03-23 14:02:59 +0100

    save some stuff

--- R/dollar.R
+++ R/dollar.R
@@ -1,9 +1,25 @@
-# `$.tidypolars` <- function(x, name, ...) {
-#   print(list2(...))
+# `$.tidypolars` <- function(x, name) {
+#   # browser()
+#   # foo <- as_quosure(frame_call(), env = caller_env())
+#   # foo2 <- enexpr(foo)
+#   # print(foo2)
+#   foo <- frame_call()
+#   print(foo)
 #   browser()
-#   x <- as_quosure(frame_call(), env = caller_env())
-#   y <- enexpr(x)
-#   x
+#   foo[[2]] |> eval_bare(env = caller_env()) |> print()
+#   # browser()
+#   # out <- eval_bare(foo, env = caller_env())
+#   # print(out)
+#   # attr(out, "polars_expressions") <- append(attr(out, "polars_expressions"), foo)
+#   nm <- if (inherits(x, "RPolarsDataFrame")) {
+#     "RPolarsDataFrame"
+#   } else if (inherits(x, "RPolarsLazyFrame")) {
+#     "RPolarsLazyFrame"
+#   }
+#   out <- NextMethod("$", nm)
+#   print(out)
+#   attr(out, "polars_expressions") <- append(attr(out, "polars_expressions"), foo)
+#   return(out)
 # }
 
 #' @export
