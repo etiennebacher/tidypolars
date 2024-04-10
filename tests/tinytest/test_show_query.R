f9142e4e5ebd6a21a1658f7a2f49fe08afd4b0db
etiennebacher <etienne.bacher@protonmail.com>
2024-04-10 21:58:04 +0200

    setup more snapshots [skip ci]

--- tests/tinytest/test_show_query.R
+++ tests/tinytest/test_show_query.R
@@ -2,12 +2,53 @@ source("helpers.R")
 using("tidypolars")
 
 expect_snapshot_print(
-  label = "simple_query",
+  label = "simple_filter_query",
   {
     iris |>
       as_polars_df() |>
-      filter(Sepal.Length > 5) |>
+      filter(Sepal.Length > 5, Species %in% c("setosa", "virginica")) |>
       show_query()
   }
 )
 
+expect_snapshot_print(
+  label = "simple_select_query",
+  {
+    iris |>
+      as_polars_df() |>
+      select(starts_with("Sep"), matches("ies$")) |>
+      show_query()
+  }
+)
+
+expect_snapshot_print(
+  label = "simple_mutate_query",
+  {
+    iris |>
+      as_polars_df() |>
+      mutate(x = (Petal.Length / Petal.Width) > 3) |>
+      show_query()
+  }
+)
+
+expect_snapshot_print(
+  label = "simple_arrange_query",
+  {
+    iris |>
+      as_polars_df() |>
+      arrange(Sepal.Length, -Sepal.Width, desc(Species)) |>
+      show_query()
+  }
+)
+
+expect_snapshot_print(
+  label = "complex_query",
+  {
+    tidyr::relig_income |>
+      as_polars_df() |>
+      pivot_longer(!religion, names_to = "income", values_to = "count") |>
+      drop_na() |>
+      arrange(religion, count) |>
+      show_query()
+  }
+)
