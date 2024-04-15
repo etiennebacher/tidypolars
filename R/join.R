a4f00b9ce86ffea9836daeb609977b8077dcc14e
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2024-04-15 18:27:42 +0200

    Implement argument `relationship` for joins (#106)
    * init
    
    * typo

--- R/join.R
+++ R/join.R
@@ -25,6 +25,14 @@
 #' character vector of length 2.
 #' @inheritParams slice_tail.RPolarsDataFrame
 #' @param copy,keep Not used.
+#' @param relationship Handling of the expected relationship between the keys of
+#' `x` and `y`. Must be one of the following:
+#' * `NULL`, the default, is equivalent to `"many-to-many"`. It doesn't expect
+#'   any relationship between `x` and `y`.
+#' * `"one-to-one"` expects each row in `x` to match at most 1 row in `y` and
+#'   each row in `y` to match at most 1 row in `x`.
+#' * `"one-to-many"` expects each row in `y` to match at most 1 row in `x`.
+#' * `"many-to-one"` expects each row in `x` matches at most 1 row in `y`.
 #'
 #' @export
 #' @examplesIf require("dplyr", quietly = TRUE) && require("tidyr", quietly = TRUE)
@@ -61,35 +69,63 @@
 #' )
 #'
 #' left_join(test, test2, by = "x", suffix = c("_left", "_right"))
+#'
+#' # the argument "relationship" ensures the join matches the expectation
+#' country <- polars::pl$DataFrame(
+#'   iso = c("FRA", "DEU"),
+#'   value = 1:2
+#' )
+#' country
+#'
+#' country_year <- polars::pl$DataFrame(
+#'   iso = rep(c("FRA", "DEU"), each = 2),
+#'   year = rep(2019:2020, 2),
+#'   value2 = 3:6
+#' )
+#' country_year
+#'
+#' # We expect that each row in "x" matches only one row in "y" but, it's not
+#' # true as each row of "x" matches two rows of "y"
+#' tryCatch(
+#'   left_join(country, country_year, join_by(iso), relationship = "one-to-one"),
+#'   error = function(e) e
+#' )
+#'
+#' # A correct expectation would be "one-to-many":
+#' left_join(country, country_year, join_by(iso), relationship = "one-to-many")
 
 left_join.RPolarsDataFrame <- function(x, y, by = NULL, copy = NULL,
-                                suffix = c(".x", ".y"), ..., keep = NULL) {
+                                       suffix = c(".x", ".y"), ..., keep = NULL,
+                                       relationship = NULL) {
   unused_args(copy, keep)
-  join_(x = x, y = y, by = by, how = "left", suffix = suffix)
+  join_(x = x, y = y, by = by, how = "left", suffix = suffix, relationship = relationship)
 }
 
 #' @rdname left_join.RPolarsDataFrame
 #' @export
 right_join.RPolarsDataFrame <- function(x, y, by = NULL, copy = NULL,
-                                 suffix = c(".x", ".y"), ..., keep = NULL) {
+                                        suffix = c(".x", ".y"), ..., keep = NULL,
+                                        relationship = NULL) {
   unused_args(copy, keep)
-  join_(x = x, y = y, by = by, how = "right", suffix = suffix)
+  join_(x = x, y = y, by = by, how = "right", suffix = suffix, relationship = relationship)
 }
 
 #' @rdname left_join.RPolarsDataFrame
 #' @export
 full_join.RPolarsDataFrame <- function(x, y, by = NULL, copy = NULL,
-                                suffix = c(".x", ".y"), ..., keep = NULL) {
+                                       suffix = c(".x", ".y"), ..., keep = NULL,
+                                       relationship = NULL) {
   unused_args(copy, keep)
-  join_(x = x, y = y, by = by, how = "outer_coalesce", suffix = suffix)
+  join_(x = x, y = y, by = by, how = "outer_coalesce", suffix = suffix, relationship = relationship)
 }
 
 #' @rdname left_join.RPolarsDataFrame
 #' @export
 inner_join.RPolarsDataFrame <- function(x, y, by = NULL, copy = NULL,
-                                 suffix = c(".x", ".y"), ..., keep = NULL) {
+                                        suffix = c(".x", ".y"), ..., keep = NULL,
+                                        relationship = NULL) {
   unused_args(copy, keep)
-  join_(x = x, y = y, by = by, how = "inner", suffix = suffix)
+  join_(x = x, y = y, by = by, how = "inner", suffix = suffix, relationship = relationship)
 }
 
 #' @rdname left_join.RPolarsDataFrame
@@ -125,32 +161,31 @@ inner_join.RPolarsLazyFrame <- inner_join.RPolarsDataFrame
 #'   y = c(1, 2, 3),
 #'   z = c(1, 2, 3)
 #' )
-#
+#'
 #' test2 <- polars::pl$DataFrame(
 #'   x = c(1, 2, 4),
 #'   y = c(1, 2, 4),
 #'   z2 = c(1, 2, 4)
 #' )
-#
+#'
 #' test
-#
+#'
 #' test2
-#
+#'
 #' # only keep the rows of `test` that have matching keys in `test2`
 #' semi_join(test, test2, by = c("x", "y"))
-#
+#'
 #' # only keep the rows of `test` that don't have matching keys in `test2`
 #' anti_join(test, test2, by = c("x", "y"))
-
 semi_join.RPolarsDataFrame <- function(x, y, by = NULL, ...) {
-  join_(x = x, y = y, by = by, how = "semi", suffix = NULL)
+  join_(x = x, y = y, by = by, how = "semi", suffix = NULL, relationship = NULL)
 }
 
 #' @rdname semi_join.RPolarsDataFrame
 #' @export
 
 anti_join.RPolarsDataFrame <- function(x, y, by = NULL, ...) {
-  join_(x = x, y = y, by = by, how = "anti", suffix = NULL)
+  join_(x = x, y = y, by = by, how = "anti", suffix = NULL, relationship = NULL)
 }
 
 #' @rdname semi_join.RPolarsDataFrame
@@ -185,9 +220,8 @@ anti_join.RPolarsLazyFrame <- anti_join.RPolarsDataFrame
 #' test2
 #'
 #' cross_join(test, test2)
-
 cross_join.RPolarsDataFrame <- function(x, y, suffix = c(".x", ".y"), ...) {
-  join_(x = x, y = y, by = NULL, how = "cross", suffix = suffix)
+  join_(x = x, y = y, by = NULL, how = "cross", suffix = suffix, relationship = NULL)
 }
 
 #' @rdname cross_join.RPolarsDataFrame
@@ -195,10 +229,10 @@ cross_join.RPolarsDataFrame <- function(x, y, suffix = c(".x", ".y"), ...) {
 cross_join.RPolarsLazyFrame <- cross_join.RPolarsDataFrame
 
 
-join_ <- function(x, y, by = NULL, how, suffix) {
+join_ <- function(x, y, by = NULL, how, suffix, relationship) {
   all_df_or_lf <-
     all(vapply(list(x, y), inherits, what = "RPolarsDataFrame", FUN.VALUE = logical(1L))) ||
-    all(vapply(list(x, y), inherits, what = "RPolarsLazyFrame", FUN.VALUE = logical(1L)))
+      all(vapply(list(x, y), inherits, what = "RPolarsLazyFrame", FUN.VALUE = logical(1L)))
 
   if (!all_df_or_lf) {
     rlang::abort(
@@ -212,6 +246,22 @@ join_ <- function(x, y, by = NULL, how, suffix) {
       "`suffix` must be of length 2.",
       call = caller_env()
     )
+  }
+
+  if (!is.null(relationship)) {
+    validate <- switch(
+      relationship,
+      "many-to-many" = "m:m",
+      "one-to-one" = "1:1",
+      "many-to-one" = "m:1",
+      "one-to-many" = "1:m",
+      abort(
+        paste0("`relationship` must be one of \"one-to-one\", \"one-to-many\", \"many-to-one\", or \"many-to-many\", not \"", relationship, "\"."),
+        call = caller_env()
+      )
+    )
+  } else {
+    validate <- "m:m"
   }
 
   if (is.null(by) && how != "cross") {
@@ -253,9 +303,9 @@ join_ <- function(x, y, by = NULL, how, suffix) {
   )
 
   if (how == "right") {
-    out <- y$join(other = x, left_on = left_on, right_on = right_on, how = "left")
+    out <- y$join(other = x, left_on = left_on, right_on = right_on, how = "left", validate = validate)
   } else {
-    out <- x$join(other = y, left_on = left_on, right_on = right_on, how = how)
+    out <- x$join(other = y, left_on = left_on, right_on = right_on, how = how, validate = validate)
   }
 
   out <- if (length(dupes) > 0) {
