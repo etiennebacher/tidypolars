d0422d2ea5e05885f52f18fd81ce94fb5f7fab3f
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2024-05-09 14:05:59 +0100

    add tests

---
+++ tests/tinytest/test_explicit_namespace.R
@@ -0,0 +1,15 @@
+source("helpers.R")
+using("tidypolars")
+
+test <- polars::pl$DataFrame(x = 1:3)
+
+expect_equal(
+  test |> mutate(y = lag(x)),
+  test |> mutate(y = dplyr::lag(x))
+)
+
+# function exists but has no translation
+expect_error(
+  test |> mutate(y = data.table::shift(x)),
+  "doesn't know how to translate this function"
+)
---
+++ tests/tinytest/test_explicit_namespace_lazy.R
@@ -0,0 +1,21 @@
+### [GENERATED AUTOMATICALLY] Update test_explicit_namespace.R instead.
+
+Sys.setenv('TIDYPOLARS_TEST' = TRUE)
+
+source("helpers.R")
+using("tidypolars")
+
+test <- polars::pl$LazyFrame(x = 1:3)
+
+expect_equal_lazy(
+  test |> mutate(y = lag(x)),
+  test |> mutate(y = dplyr::lag(x))
+)
+
+# function exists but has no translation
+expect_error_lazy(
+  test |> mutate(y = data.table::shift(x)),
+  "doesn't know how to translate this function"
+)
+
+Sys.setenv('TIDYPOLARS_TEST' = FALSE)
