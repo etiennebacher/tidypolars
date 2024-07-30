e4e447c686e7329173140ca89690aca77704c084
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2024-07-30 20:09:17 +0200

    Deprecate describe(), use summary() instead (#127)

--- R/describe.R
+++ R/describe.R
@@ -1,26 +1,16 @@
-#' Summary statistics for a Polars DataFrame
+#' @inherit summary.RPolarsDataFrame title params
 #'
-#' @inheritParams complete.RPolarsDataFrame
-#' @param percentiles One or more percentiles to include in the summary
-#' statistics. All values must be between 0 and 1 (`NULL` are ignored).
+#' @description
+#' This function is deprecated as of tidypolars 0.10.0, it will be removed in
+#' future update. Use `summary()` with the same arguments instead.
 #'
 #' @export
-#' @examplesIf require("dplyr", quietly = TRUE) && require("tidyr", quietly = TRUE)
-#' mtcars |>
-#'   as_polars_df() |>
-#'   describe(percentiles = c(0.2, 0.4, 0.6, 0.8))
 
 describe <- function(.data, percentiles = c(0.25, 0.75)) {
   if (!inherits(.data, "RPolarsDataFrame")) {
     rlang::abort("`describe()` can only be used on a DataFrame.")
   }
-  between_zero_one <- percentiles >= 0 & percentiles <= 1
-  if (anyNA(between_zero_one) || any(!between_zero_one)) {
-    abort(
-      "All values of `percentiles` must be between 0 and 1."
-    )
-  }
-  out <- .data$describe(percentiles)
-  add_tidypolars_class(out)
+  rlang::warn("`describe()` is deprecated as of tidypolars 0.10.0 and will be removed in future update.\nUse `summary()` with the same arguments instead.")
+  summary(object = .data, percentiles = percentiles)
 }
 
--- tests/tinytest/test_describe.R
+++ tests/tinytest/test_describe.R
@@ -3,38 +3,12 @@ using("tidypolars")
 
 test <- pl$DataFrame(mtcars)
 
-expect_is_tidypolars(describe(test))
-
-expect_equal(
-  describe(test) |>
-    pull(statistic),
-  c("count", "null_count", "mean", "std", "min", "25%", "50%", "75%", "max")
+expect_warning(
+  expect_equal(
+    describe(test) |>
+      pull(statistic),
+    c("count", "null_count", "mean", "std", "min", "25%", "50%", "75%", "max")
+  ),
+  "Use `summary()` with the same arguments instead.",
+  fixed = TRUE
 )
-
-expect_equal(
-  describe(test, percentiles = c(0.2, 0.4)) |>
-    pull(statistic),
-  c("count", "null_count", "mean", "std", "min", "20%", "40%", "50%", "max")
-)
-
-expect_equal(
-  describe(test, percentiles = c(0.2, 0.4, NULL)) |>
-    pull(statistic),
-  c("count", "null_count", "mean", "std", "min", "20%", "40%", "50%", "max")
-)
-
-expect_error(
-  describe(test, percentiles = c(0.2, 0.4, NA)),
-  'between 0 and 1'
-)
-
-expect_error(
-  describe(test, percentiles = -1),
-  'between 0 and 1'
-)
-
-expect_error(
-  describe(test$lazy(), percentiles = 0.3),
-  'only be used on a DataFrame'
-)
-
