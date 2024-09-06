57294e1cc34da3d8ad1cf84b51175b9e67c07783
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2024-09-06 13:04:39 +0200

    PoC for hypothesis testing

---
+++ tests/testthat/test-funs-string.R
@@ -0,0 +1,75 @@
+test_that("str_to_lower() works", {
+  for_all(
+    tests = 20,
+    string = character_(),
+    property = function(string, fun) {
+      test_df <- data.frame(x1 = string)
+      test <- pl$DataFrame(x1 = string)
+
+      testthat::expect_equal(
+        mutate(test, foo = str_to_lower(x1)) |>
+          pull(foo),
+        mutate(test_df, foo = str_to_lower(x1)) |>
+          pull(foo)
+      )
+    }
+  )
+})
+
+test_that("str_to_upper() works", {
+  for_all(
+    tests = 20,
+    string = character_(),
+    property = function(string, fun) {
+      test_df <- data.frame(x1 = string)
+      test <- pl$DataFrame(x1 = string)
+
+      testthat::expect_equal(
+        mutate(test, foo = str_to_upper(x1)) |>
+          pull(foo),
+        mutate(test_df, foo = str_to_upper(x1)) |>
+          pull(foo)
+      )
+    }
+  )
+})
+
+test_that("paste() and paste0() work", {
+  for_all(
+    tests = 20,
+    string = character_(),
+    property = function(string, fun) {
+      test_df <- data.frame(x1 = string)
+      test <- pl$DataFrame(x1 = string)
+
+      testthat::expect_equal(
+        mutate(test, foo = paste(x1, "he")) |>
+          pull(foo),
+        mutate(test_df, foo = paste(x1, "he")) |>
+          pull(foo)
+      )
+
+      testthat::expect_equal(
+        mutate(test, foo = paste(x1, "he", sep = "--")) |>
+          pull(foo),
+        mutate(test_df, foo = paste(x1, "he", sep = "--")) |>
+          pull(foo)
+      )
+
+      testthat::expect_equal(
+        mutate(test, foo = paste0(x1, "he")) |>
+          pull(foo),
+        mutate(test_df, foo = paste0(x1, "he")) |>
+          pull(foo)
+      )
+
+      testthat::expect_equal(
+        mutate(test, foo = paste0(x1, "he", x1)) |>
+          pull(foo),
+        mutate(test_df, foo = paste0(x1, "he", x1)) |>
+          pull(foo)
+      )
+    }
+  )
+})
+
