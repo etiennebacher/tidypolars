9e13a6ff6d6dfb946acad5a4589c033679bfb914
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2024-09-06 13:34:42 +0200

    ...

--- tests/testthat.R
+++ tests/testthat.R
@@ -6,7 +6,7 @@
 # * https://r-pkgs.org/testing-design.html#sec-tests-files-overview
 # * https://testthat.r-lib.org/articles/special-files.html
 
-librayr(quickcheck)
+library(quickcheck)
 library(testthat)
 library(tidypolars)
 
