41421ae3ea326d447310125dc2696cdc62a1aef0
Etienne Bacher <52219252+etiennebacher@users.noreply.github.com>
2024-09-23 19:19:06 +0200

    more [skip ci]

---
+++ tests/testthat/test-join_filtering-lazy.R
@@ -0,0 +1,153 @@
+### [GENERATED AUTOMATICALLY] Update test-join_filtering.R instead.
+
+Sys.setenv('TIDYPOLARS_TEST' = TRUE)
+
+test_that("basic behavior with common column names", {
+  test <- polars::pl$LazyFrame(
+    x = c(1, 2, 3),
+    y = c(1, 2, 3),
+    z = c(1, 2, 3)
+  )
+  test2 <- polars::pl$LazyFrame(
+    x = c(1, 2, 4),
+    y = c(1, 2, 4),
+    z2 = c(1, 2, 4)
+  )
+
+  expect_is_tidypolars(semi_join(test, test2, join_by(x, y)))
+  expect_is_tidypolars(anti_join(test, test2, join_by(x, y)))
+
+  expect_equal_lazy(
+    semi_join(test, test2, by = c("x", "y")),
+    pl$LazyFrame(
+      x = c(1, 2), y = c(1, 2), z = c(1, 2)
+    )
+  )
+
+  expect_equal_lazy(
+    anti_join(test, test2, by = c("x", "y")),
+    pl$LazyFrame(
+      x = 3, y = 3, z = 3
+    )
+  )
+})
+
+
+test_that("basic behavior with different column names", {
+  test <- polars::pl$LazyFrame(
+    x = c(1, 2, 3),
+    y1 = c(1, 2, 3),
+    z = c(1, 2, 3)
+  )
+  test2 <- polars::pl$LazyFrame(
+    x = c(1, 2, 4),
+    y2 = c(1, 2, 4),
+    z2 = c(1, 2, 4)
+  )
+
+  expect_equal_lazy(
+    semi_join(test, test2, by = c("x", "y1" = "y2")),
+    pl$LazyFrame(
+      x = c(1, 2), y1 = c(1, 2), z = c(1, 2)
+    )
+  )
+
+  expect_equal_lazy(
+    anti_join(test, test2, by = c("x", "y1" = "y2")),
+    pl$LazyFrame(
+      x = 3, y1 = 3, z = 3
+    )
+  )
+})
+
+test_that("join_by() with strict equality", {
+  test <- polars::pl$LazyFrame(
+    x = c(1, 2, 3),
+    y1 = c(1, 2, 3),
+    z = c(1, 2, 3)
+  )
+  test2 <- polars::pl$LazyFrame(
+    x = c(1, 2, 4),
+    y2 = c(1, 2, 4),
+    z2 = c(1, 2, 4)
+  )
+
+  expect_equal_lazy(
+    semi_join(test, test2, by = join_by(x, y1 == y2)),
+    pl$LazyFrame(
+      x = c(1, 2), y1 = c(1, 2), z = c(1, 2)
+    )
+  )
+
+  expect_equal_lazy(
+    anti_join(test, test2, by = join_by(x, y1 == y2)),
+    pl$LazyFrame(
+      x = 3, y1 = 3, z = 3
+    )
+  )
+})
+
+test_that("join_by() doesn't work with inequality", {
+  test <- polars::pl$LazyFrame(
+    x = c(1, 2, 3),
+    y1 = c(1, 2, 3),
+    z = c(1, 2, 3)
+  )
+  test2 <- polars::pl$LazyFrame(
+    x = c(1, 2, 4),
+    y2 = c(1, 2, 4),
+    z2 = c(1, 2, 4)
+  )
+
+  expect_error_lazy(
+    semi_join(test, test2, by = join_by(x, y1 > y2)),
+    "doesn't support inequality conditions"
+  )
+
+  expect_error_lazy(
+    anti_join(test, test2, by = join_by(x, y1 > y2)),
+    "doesn't support inequality conditions"
+  )
+})
+
+test_that("fallback on dplyr error if wrong join_by specification", {
+  test <- polars::pl$LazyFrame(
+    x = c(1, 2, 3),
+    y1 = c(1, 2, 3),
+    z = c(1, 2, 3)
+  )
+  test2 <- polars::pl$LazyFrame(
+    x = c(1, 2, 4),
+    y2 = c(1, 2, 4),
+    z2 = c(1, 2, 4)
+  )
+
+  expect_error_lazy(
+    semi_join(test, test2, by = join_by(x, y1 = y2)),
+    "Can't name join expressions"
+  )
+
+  expect_error_lazy(
+    anti_join(test, test2, by = join_by(x, y1 = y2)),
+    "Can't name join expressions"
+  )
+})
+
+test_that("argument na_matches works", {
+  pdf1 <- pl$LazyFrame(a = c(1, NA, NA, NaN), val = 1:4)
+  pdf2 <- pl$LazyFrame(a = c(1, 2, NA, NaN), val2 = 5:8)
+
+  expect_equal_lazy(
+    semi_join(pdf1, pdf2, "a") |>
+      pull(a),
+    c(1, NA, NA, NaN)
+  )
+
+  expect_equal_lazy(
+    semi_join(pdf1, pdf2, "a", na_matches = "never") |>
+      pull(a),
+    c(1, NaN)
+  )
+})
+
+Sys.setenv('TIDYPOLARS_TEST' = FALSE)
