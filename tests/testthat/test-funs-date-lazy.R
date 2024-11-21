b259f27269b6126f58c215b23c1ff79d8c9597ad
etiennebacher <etienne.bacher@protonmail.com>
2024-11-21 11:43:37 +0100

    misc: add more tests for extraction of date components, document those functions

--- tests/testthat/test-funs-date-lazy.R
+++ tests/testthat/test-funs-date-lazy.R
@@ -3,26 +3,43 @@
 Sys.setenv('TIDYPOLARS_TEST' = TRUE)
 
 patrick::with_parameters_test_that(
-  "extracting date components works", {
+  "extracting date components works",
+  {
     for_all(
       tests = 20,
-      date = date_(),
-      property = function(date) {
+      date = date_(any_na = TRUE),
+      datetime = posixct_(any_na = TRUE),
+      property = function(date, datetime) {
+        # date -----------------------------------
         test_df <- data.frame(x1 = date)
         test <- pl$LazyFrame(x1 = date)
 
-        pl_code <- paste0("mutate(test, foo = ", fun, "(date)) |> pull(foo)")
-        tv_code <- paste0("mutate(test_df, foo = ", fun, "(date)) |> pull(foo)")
+        pl_code <- paste0("mutate(test, foo = ", fun, "(x1)) |> pull(foo)")
+        tv_code <- paste0("mutate(test_df, foo = ", fun, "(x1)) |> pull(foo)")
 
         expect_equal_lazy(
           eval(parse(text = pl_code)),
-          eval(parse(text = tv_code)),
+          eval(parse(text = tv_code))
         )
+
+        # datetime -----------------------------------
+        # TODO: this should be removed eventually
+        if (!fun %in% c("yday", "mday")) {
+          test_df <- data.frame(x1 = datetime)
+          test <- pl$LazyFrame(x1 = datetime)
+
+          pl_code <- paste0("mutate(test, foo = ", fun, "(x1)) |> pull(foo)")
+          tv_code <- paste0("mutate(test_df, foo = ", fun, "(x1)) |> pull(foo)")
+
+          expect_equal_lazy(
+            eval(parse(text = pl_code)),
+            eval(parse(text = tv_code))
+          )
+        }
       }
     )
   },
   fun = c("year", "month", "day", "quarter", "mday", "yday")
 )
-
 
 Sys.setenv('TIDYPOLARS_TEST' = FALSE)
