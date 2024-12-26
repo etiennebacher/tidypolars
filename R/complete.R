52359fc9df953bdf14b58a848cfa83b7380863ba
etiennebacher <etienne.bacher@protonmail.com>
2024-12-26 21:32:57 +0100

    format

--- R/complete.R
+++ R/complete.R
@@ -46,107 +46,109 @@
 #' df |>
 #'   group_by(group, maintain_order = TRUE) |>
 #'   complete(item_id, item_name)
-complete.RPolarsDataFrame <- function(data,
-                                      ...,
-                                      fill = list(),
-                                      explicit = TRUE) {
-  # if (isFALSE(explicit)) {
-  #   abort("`explicit = FALSE` is not supported by tidypolars.")
-  # }
-  vars <- tidyselect_dots(data, ...)
-  if (length(vars) < 2) {
-    return(data)
-  }
+complete.RPolarsDataFrame <- function(
+	data,
+	...,
+	fill = list(),
+	explicit = TRUE
+) {
+	# if (isFALSE(explicit)) {
+	#   abort("`explicit = FALSE` is not supported by tidypolars.")
+	# }
+	vars <- tidyselect_dots(data, ...)
+	if (length(vars) < 2) {
+		return(data)
+	}
 
-  grps <- attributes(data)$pl_grps
-  mo <- attributes(data)$maintain_grp_order
-  is_grouped <- !is.null(grps)
+	grps <- attributes(data)$pl_grps
+	mo <- attributes(data)$maintain_grp_order
+	is_grouped <- !is.null(grps)
 
-  if (isTRUE(is_grouped)) {
-    chain <- data$group_by(grps, maintain_order = mo)$agg(
-      pl$col(vars)$unique()$sort()
-    )
-  } else {
-    chain <- data$select(pl$col(vars)$unique()$sort()$implode())
-  }
+	if (isTRUE(is_grouped)) {
+		chain <- data$group_by(grps, maintain_order = mo)$agg(
+			pl$col(vars)$unique()$sort()
+		)
+	} else {
+		chain <- data$select(pl$col(vars)$unique()$sort()$implode())
+	}
 
-  for (i in seq_along(vars)) {
-    chain <- chain$explode(vars[i])
-  }
+	for (i in seq_along(vars)) {
+		chain <- chain$explode(vars[i])
+	}
 
-  # To avoid filling explicit missings, i.e. missings that were already in the
-  # original data, I split the expanded combinations between those that were
-  # already in the data and those that weren't, and I only fill the latter.
-  if (isFALSE(explicit)) {
-    if (isTRUE(is_grouped)) {
-      already_exist <- chain$join(
-        data,
-        on = c(grps, vars),
-        how = "inner",
-        join_nulls = TRUE
-      )
-      dont_already_exist <- chain$join(
-        data,
-        on = c(grps, vars),
-        how = "anti",
-        join_nulls = TRUE
-      )
-    } else {
-      already_exist <- chain$join(
-        data,
-        on = vars,
-        how = "inner",
-        join_nulls = TRUE
-      )
-      dont_already_exist <- chain$join(
-        data,
-        on = vars,
-        how = "anti",
-        join_nulls = TRUE
-      )
-    }
-    other_vars <- setdiff(names(already_exist), names(dont_already_exist))
-    schema <- already_exist$schema
-    for (i in other_vars) {
-      dont_already_exist <- dont_already_exist$with_columns(
-        pl$lit(NA)$cast(schema[[i]])$alias(i)
-      )
-    }
-    dont_already_exist <- replace_na(dont_already_exist, fill)
-    out <- bind_rows_polars(already_exist, dont_already_exist)
-    if (isTRUE(mo)) {
-      if (isTRUE(is_grouped)) {
-        out <- out$sort(c(grps, vars))
-      } else {
-        out <- out$sort(vars)
-      }
-    }
-  } else {
-    if (isTRUE(is_grouped)) {
-      out <- chain$join(
-        data,
-        on = c(grps, vars),
-        how = "left",
-        join_nulls = TRUE
-      )
-    } else {
-      out <- chain$join(data, on = vars, how = "left", join_nulls = TRUE)
-    }
+	# To avoid filling explicit missings, i.e. missings that were already in the
+	# original data, I split the expanded combinations between those that were
+	# already in the data and those that weren't, and I only fill the latter.
+	if (isFALSE(explicit)) {
+		if (isTRUE(is_grouped)) {
+			already_exist <- chain$join(
+				data,
+				on = c(grps, vars),
+				how = "inner",
+				join_nulls = TRUE
+			)
+			dont_already_exist <- chain$join(
+				data,
+				on = c(grps, vars),
+				how = "anti",
+				join_nulls = TRUE
+			)
+		} else {
+			already_exist <- chain$join(
+				data,
+				on = vars,
+				how = "inner",
+				join_nulls = TRUE
+			)
+			dont_already_exist <- chain$join(
+				data,
+				on = vars,
+				how = "anti",
+				join_nulls = TRUE
+			)
+		}
+		other_vars <- setdiff(names(already_exist), names(dont_already_exist))
+		schema <- already_exist$schema
+		for (i in other_vars) {
+			dont_already_exist <- dont_already_exist$with_columns(
+				pl$lit(NA)$cast(schema[[i]])$alias(i)
+			)
+		}
+		dont_already_exist <- replace_na(dont_already_exist, fill)
+		out <- bind_rows_polars(already_exist, dont_already_exist)
+		if (isTRUE(mo)) {
+			if (isTRUE(is_grouped)) {
+				out <- out$sort(c(grps, vars))
+			} else {
+				out <- out$sort(vars)
+			}
+		}
+	} else {
+		if (isTRUE(is_grouped)) {
+			out <- chain$join(
+				data,
+				on = c(grps, vars),
+				how = "left",
+				join_nulls = TRUE
+			)
+		} else {
+			out <- chain$join(data, on = vars, how = "left", join_nulls = TRUE)
+		}
 
-    if (length(fill) > 0) {
-      out <- replace_na(out, fill)
-    }
-  }
+		if (length(fill) > 0) {
+			out <- replace_na(out, fill)
+		}
+	}
 
-  out <- if (is_grouped) {
-    out |>
-      relocate(all_of(grps), .before = 1) |>
-      group_by(all_of(grps), maintain_order = mo)
-  } else {
-    out
-  }
+	out <- if (is_grouped) {
+		out |>
+			relocate(all_of(grps), .before = 1) |>
+			group_by(all_of(grps), maintain_order = mo)
+	} else {
+		out
+	}
 
-  add_tidypolars_class(out)
+	add_tidypolars_class(out)
 }
 
 #' @rdname complete.RPolarsDataFrame
--- tests/testthat/test-funs-string.R
+++ tests/testthat/test-funs-string.R
@@ -1,98 +1,98 @@
 test_that("paste() and paste0() work", {
-  for_all(
-    tests = 40,
-    string = character_(any_na = TRUE),
-    separator = character_(len = 1),
-    property = function(string, separator) {
-      test_df <- data.frame(x1 = string)
-      test <- pl$DataFrame(x1 = string)
+	for_all(
+		tests = 40,
+		string = character_(any_na = TRUE),
+		separator = character_(len = 1),
+		property = function(string, separator) {
+			test_df <- data.frame(x1 = string)
+			test <- pl$DataFrame(x1 = string)
 
-      expect_equal(
-        mutate(test, foo = paste(x1, "he")) |>
-          pull(foo),
-        mutate(test_df, foo = paste(x1, "he")) |>
-          pull(foo)
-      )
+			expect_equal(
+				mutate(test, foo = paste(x1, "he")) |>
+					pull(foo),
+				mutate(test_df, foo = paste(x1, "he")) |>
+					pull(foo)
+			)
 
-      expect_equal(
-        mutate(test, foo = paste(x1, "he", sep = separator)) |>
-          pull(foo),
-        mutate(test_df, foo = paste(x1, "he", sep = separator)) |>
-          pull(foo)
-      )
+			expect_equal(
+				mutate(test, foo = paste(x1, "he", sep = separator)) |>
+					pull(foo),
+				mutate(test_df, foo = paste(x1, "he", sep = separator)) |>
+					pull(foo)
+			)
 
-      expect_equal(
-        mutate(test, foo = paste0(x1, "he")) |>
-          pull(foo),
-        mutate(test_df, foo = paste0(x1, "he")) |>
-          pull(foo)
-      )
+			expect_equal(
+				mutate(test, foo = paste0(x1, "he")) |>
+					pull(foo),
+				mutate(test_df, foo = paste0(x1, "he")) |>
+					pull(foo)
+			)
 
-      expect_equal(
-        mutate(test, foo = paste0(x1, "he", x1)) |>
-          pull(foo),
-        mutate(test_df, foo = paste0(x1, "he", x1)) |>
-          pull(foo)
-      )
-    }
-  )
+			expect_equal(
+				mutate(test, foo = paste0(x1, "he", x1)) |>
+					pull(foo),
+				mutate(test_df, foo = paste0(x1, "he", x1)) |>
+					pull(foo)
+			)
+		}
+	)
 })
 
 patrick::with_parameters_test_that(
-  "several non-regex functions work",
-  {
-    for_all(
-      tests = 40,
-      string = character_(any_na = TRUE),
-      property = function(string) {
-        test_df <- data.frame(x1 = string)
-        test <- pl$DataFrame(x1 = string)
+	"several non-regex functions work",
+	{
+		for_all(
+			tests = 40,
+			string = character_(any_na = TRUE),
+			property = function(string) {
+				test_df <- data.frame(x1 = string)
+				test <- pl$DataFrame(x1 = string)
 
-        pl_code <- paste0("mutate(test, foo = ", fun, "(string)) |> pull(foo)")
-        tv_code <- paste0(
-          "mutate(test_df, foo = ",
-          fun,
-          "(string)) |> pull(foo)"
-        )
+				pl_code <- paste0("mutate(test, foo = ", fun, "(string)) |> pull(foo)")
+				tv_code <- paste0(
+					"mutate(test_df, foo = ",
+					fun,
+					"(string)) |> pull(foo)"
+				)
 
-        expect_equal(
-          eval(parse(text = pl_code)),
-          eval(parse(text = tv_code)),
-        )
-      }
-    )
-  },
-  fun = c("str_to_upper", "str_to_lower", "str_length", "str_squish")
+				expect_equal(
+					eval(parse(text = pl_code)),
+					eval(parse(text = tv_code)),
+				)
+			}
+		)
+	},
+	fun = c("str_to_upper", "str_to_lower", "str_length", "str_squish")
 )
 
 test_that("str_trim() works", {
-  for_all(
-    tests = 40,
-    string = character_(any_na = TRUE),
-    side = quickcheck::one_of(
-      constant("both"),
-      constant("left"),
-      constant("right")
-    ),
-    property = function(string, side) {
-      test_df <- data.frame(x1 = string)
-      test <- pl$DataFrame(x1 = string)
+	for_all(
+		tests = 40,
+		string = character_(any_na = TRUE),
+		side = quickcheck::one_of(
+			constant("both"),
+			constant("left"),
+			constant("right")
+		),
+		property = function(string, side) {
+			test_df <- data.frame(x1 = string)
+			test <- pl$DataFrame(x1 = string)
 
-      expect_equal(
-        mutate(test, foo = str_trim(x1)) |>
-          pull(foo),
-        mutate(test_df, foo = str_trim(x1)) |>
-          pull(foo)
-      )
+			expect_equal(
+				mutate(test, foo = str_trim(x1)) |>
+					pull(foo),
+				mutate(test_df, foo = str_trim(x1)) |>
+					pull(foo)
+			)
 
-      expect_equal(
-        mutate(test, foo = str_trim(x1, side = side)) |>
-          pull(foo),
-        mutate(test_df, foo = str_trim(x1, side = side)) |>
-          pull(foo)
-      )
-    }
-  )
+			expect_equal(
+				mutate(test, foo = str_trim(x1, side = side)) |>
+					pull(foo),
+				mutate(test_df, foo = str_trim(x1, side = side)) |>
+					pull(foo)
+			)
+		}
+	)
 })
 
 # TODO: Problem is that I don't have a way to check that length of string is the
@@ -130,66 +130,66 @@ test_that("str_trim() works", {
 # })
 
 test_that("str_dup() works", {
-  for_all(
-    tests = 100,
-    string = character_(any_na = TRUE),
-    # Very high numbers crash the session, I guess because of stringr
-    times = numeric_bounded(-10000, 10000, any_na = TRUE),
-    property = function(string, times) {
-      test_df <- data.frame(x1 = string)
-      test <- pl$DataFrame(x1 = string)
+	for_all(
+		tests = 100,
+		string = character_(any_na = TRUE),
+		# Very high numbers crash the session, I guess because of stringr
+		times = numeric_bounded(-10000, 10000, any_na = TRUE),
+		property = function(string, times) {
+			test_df <- data.frame(x1 = string)
+			test <- pl$DataFrame(x1 = string)
 
-      expect_equal_or_both_error(
-        mutate(test, foo = str_dup(x1, times = times)) |>
-          pull(foo),
-        mutate(test_df, foo = str_dup(x1, times = times)) |>
-          pull(foo)
-      )
-    }
-  )
+			expect_equal_or_both_error(
+				mutate(test, foo = str_dup(x1, times = times)) |>
+					pull(foo),
+				mutate(test_df, foo = str_dup(x1, times = times)) |>
+					pull(foo)
+			)
+		}
+	)
 })
 
 test_that("str_sub() works", {
-  for_all(
-    tests = 40,
-    string = character_(any_na = TRUE),
-    start = numeric_(any_na = TRUE),
-    end = numeric_(any_na = TRUE),
-    property = function(string, start, end) {
-      test_df <- data.frame(x1 = string)
-      test <- pl$DataFrame(x1 = string)
+	for_all(
+		tests = 40,
+		string = character_(any_na = TRUE),
+		start = numeric_(any_na = TRUE),
+		end = numeric_(any_na = TRUE),
+		property = function(string, start, end) {
+			test_df <- data.frame(x1 = string)
+			test <- pl$DataFrame(x1 = string)
 
-      expect_equal_or_both_error(
-        mutate(test, foo = str_sub(x1, start, end)) |>
-          pull(foo),
-        mutate(test_df, foo = str_sub(x1, start, end)) |>
-          pull(foo)
-      )
-    }
-  )
+			expect_equal_or_both_error(
+				mutate(test, foo = str_sub(x1, start, end)) |>
+					pull(foo),
+				mutate(test_df, foo = str_sub(x1, start, end)) |>
+					pull(foo)
+			)
+		}
+	)
 })
 
 test_that("substr() works", {
-  # substr() doesn't error with different lengths but polars does. I don't want
-  # this weird case to prevent quickcheck to run, especially since this is a
-  # weird behavior in base R and we're more conservative on this.
-  length <- sample(1:10, 1)
+	# substr() doesn't error with different lengths but polars does. I don't want
+	# this weird case to prevent quickcheck to run, especially since this is a
+	# weird behavior in base R and we're more conservative on this.
+	length <- sample(1:10, 1)
 
-  for_all(
-    tests = 40,
-    string = character_(any_na = TRUE, len = length),
-    start = numeric_(any_na = TRUE, len = length),
-    end = numeric_(any_na = TRUE, len = length),
-    property = function(string, start, end) {
-      test_df <- data.frame(x1 = string)
-      test <- pl$DataFrame(x1 = string)
+	for_all(
+		tests = 40,
+		string = character_(any_na = TRUE, len = length),
+		start = numeric_(any_na = TRUE, len = length),
+		end = numeric_(any_na = TRUE, len = length),
+		property = function(string, start, end) {
+			test_df <- data.frame(x1 = string)
+			test <- pl$DataFrame(x1 = string)
 
-      expect_equal_or_both_error(
-        mutate(test, foo = substr(x1, start, end)) |>
-          pull(foo),
-        mutate(test_df, foo = substr(x1, start, end)) |>
-          pull(foo)
-      )
-    }
-  )
+			expect_equal_or_both_error(
+				mutate(test, foo = substr(x1, start, end)) |>
+					pull(foo),
+				mutate(test_df, foo = substr(x1, start, end)) |>
+					pull(foo)
+			)
+		}
+	)
 })
