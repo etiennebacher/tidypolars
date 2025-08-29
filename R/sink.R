e2fa384f952e7204fb9f7ef46d52ac8956a88316
etiennebacher <etienne.bacher@protonmail.com>
2025-08-29 14:03:37 +0200

    docs for partition functions

--- R/sink.R
+++ R/sink.R
@@ -562,7 +562,25 @@ sink_ndjson <- function(
   )
 }
 
-
+#' Helper functions to export a LazyFrame as a partitioned output
+#'
+#' `r lifecycle::badge("experimental")`
+#' More details and examples in the documentation of `sink_*()` functions.
+#'
+#' @inheritParams rlang::args_dots_empty
+#' @param base_path The base path for the output files. Use the `mkdir` option
+#' of the `sink_*` methods to ensure directories in the path are created.
+#' @param by Something can be coerced to a list of Polars expressions. Used to
+#' partition by.
+#' @param include_key If `TRUE` (default), include the key columns in the output
+#' files.
+#' @param per_partition_sort_by Something can be coerced to a list of Polars
+#' expressions, or `NULL` (default). Used  to sort over within each partition.
+#' Note that this might increase the memory consumption needed for each partition.
+#' @param max_size An integer-ish value indicating the maximum number of rows in
+#' each of the generated files.
+#'
+#' @rdname partitioned_output
 #' @export
 partition_by_key <- function(
   base_path,
@@ -580,6 +598,7 @@ partition_by_key <- function(
   )
 }
 
+#' @rdname partitioned_output
 #' @export
 partition_by_max_size <- function(
   base_path,
