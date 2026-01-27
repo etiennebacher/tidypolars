5de6953355984a17203aaaeff5ba3afdf2ab7726
etiennebacher <etienne.bacher@protonmail.com>
2026-01-27 15:36:50 +0100

    bump dplyr version requirement

--- R/zzz.R
+++
@@ -1,6 +0,0 @@
-.onLoad <- function(...) {
-  if (packageVersion("dplyr") >= "1.2.0") {
-    s3_register("dplyr::filter_out", "polars_data_frame")
-    s3_register("dplyr::filter_out", "polars_lazy_frame")
-  }
-}
