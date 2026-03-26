5f105ae18553806eda42e0664182067601656f98
etiennebacher <etienne.bacher@protonmail.com>
2026-03-26 14:48:00 +0100

    more tests

--- tests/testthat/test-by.R
+++ tests/testthat/test-by.R
@@ -10,4 +10,43 @@ test_that(".by doesn't work on already grouped data", {
     test_pl |> group_by(Species) |> mutate(foo = 1, .by = Species),
     error = TRUE
   )
+
+  expect_both_error(
+    test_pl |> group_by(Species) |> filter(Sepal.Length > 5, .by = Species),
+    test_df |> group_by(Species) |> filter(Sepal.Length > 5, .by = Species)
+  )
+
+  expect_both_error(
+    test_pl |> group_by(Species) |> filter_out(Sepal.Length > 5, .by = Species),
+    test_df |> group_by(Species) |> filter_out(Sepal.Length > 5, .by = Species)
+  )
+
+  expect_both_error(
+    test_pl |> group_by(Species) |> fill(Sepal.Length, .by = Species),
+    test_df |> group_by(Species) |> fill(Sepal.Length, .by = Species)
+  )
+
+  expect_both_error(
+    test_pl |>
+      group_by(Species) |>
+      summarize(x = mean(Sepal.Length), .by = Species),
+    test_df |>
+      group_by(Species) |>
+      summarize(x = mean(Sepal.Length), .by = Species)
+  )
+
+  expect_both_error(
+    test_pl |> group_by(Species) |> slice_head(n = 2, by = Species),
+    test_df |> group_by(Species) |> slice_head(n = 2, by = Species)
+  )
+
+  expect_both_error(
+    test_pl |> group_by(Species) |> slice_tail(n = 2, by = Species),
+    test_df |> group_by(Species) |> slice_tail(n = 2, by = Species)
+  )
+
+  expect_both_error(
+    test_pl |> group_by(Species) |> slice_sample(n = 2, by = Species),
+    test_df |> group_by(Species) |> slice_sample(n = 2, by = Species)
+  )
 })
