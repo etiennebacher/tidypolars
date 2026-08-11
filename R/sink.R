8059959add3b2222bfaea280e38274b30d2202aa
etiennebacher <etienne.bacher@protonmail.com>
2026-08-11 23:27:00 +0100

    docs

--- R/sink.R
+++ R/sink.R
@@ -180,6 +180,8 @@ sink_parquet <- function(
 #' * `"non_numeric"`: This puts quotes around all fields that are non-numeric.
 #'   Namely, when writing a field that does not parse as a valid float or integer,
 #'   then quotes will be used even if they aren't strictly necessary.
+#' * `"never"`: This never puts quotes around fields, even if that results in
+#'   invalid CSV data (e.g. by not quoting strings containing the separator).
 #' @param quote `r lifecycle::badge("deprecated")` Deprecated, use `quote_char`
 #' instead.
 #' @param null_values `r lifecycle::badge("deprecated")` Deprecated, use
